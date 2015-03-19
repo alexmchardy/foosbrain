591f07dcfed73bb4e81b6a87b61a11fc47fdc143
Alex McHardy <amchardy@shutterstock.com>
2015-03-18 22:55:50 -0600

    Reads theme.txt and stores indices to filenames from that file.

--- foosbrain.ino
+++ foosbrain.ino
@@ -34,29 +34,32 @@ Adafruit_VS1053_FilePlayer musicPlayer =
 const uint8_t NAMELEN = 13;
 const uint8_t GOALROOTLEN = 40; // 3*12 + 3 ("/"s) + 1 ("\0")
 const uint8_t GOALPATHLEN = 53; // 4*12 + 4 ("/"s) + 1 ("\0")
+const uint8_t THEMEFILEPATHLEN = 53; // 4*12 + 4 ("/"s) + 1 ("\0")
 const uint8_t GOALFILEPATHLEN = 66; // 5*12 + 5 ("/"s) + 1 ("\0")
 //char buf[BUFLEN];
 const char ROOT[6] = "foos/";
-const char SETTINGS_FILENAME[13] = "settings.txt";
+const char SETTINGSFILENAME[13] = "settings.txt";
 const char THEMESDIR[8] = "themes/";
+const char THEMEFILENAME[11] = "/theme.txt";
 const char GOALDIR[7] = "/goal/";
 const char FASTGOALDIR[11] = "/goalfast/";
 const char SLOWGOALDIR[11] = "/goalslow/";
 bool fastGoalDirExists = false;
 bool slowGoalDirExists = false;
 char themeDir[NAMELEN];
+char themeFilePath[THEMEFILEPATHLEN];
 char goalRoot[GOALROOTLEN];
 char goalPath[GOALPATHLEN];
 char fastGoalPath[GOALPATHLEN];
 char slowGoalPath[GOALPATHLEN];
 char goalFilePath[GOALFILEPATHLEN];
 uint32_t goalSoundDelay = 0;
-char **goalSounds;
-char **fastGoalSounds;
-char **slowGoalSounds;
-uint8_t goalSoundsNum;
-uint8_t fastGoalSoundsNum;
-uint8_t slowGoalSoundsNum;
+uint16_t goalSoundIndices[10];
+uint16_t fastGoalSoundIndices[10];
+uint16_t slowGoalSoundIndices[10];
+uint8_t goalSoundCount;
+uint8_t fastGoalSoundCount;
+uint8_t slowGoalSoundCount;
 
 // Setup goal detection
 #define GOAL_PIN_BLACK 8
@@ -159,7 +162,7 @@ void initSoundFiles() {
     if (!SD.exists(goalRoot)) {
         Serial.println(F("Error: foos/ dir not found on SD card"));
     }
-    strncat(goalRoot, SETTINGS_FILENAME, GOALROOTLEN - strlen(goalRoot) - 1);
+    strncat(goalRoot, SETTINGSFILENAME, GOALROOTLEN - strlen(goalRoot) - 1);
     File settingsFile = SD.open(goalRoot, FILE_READ);
     if (!settingsFile.available()) {
         Serial.println(F("settings.txt not found"));
@@ -184,11 +187,20 @@ void initSoundFiles() {
     strncpy(goalRoot, ROOT, GOALROOTLEN-1);
     strncat(goalRoot, THEMESDIR, GOALROOTLEN - strlen(goalRoot) - 1);
     strncat(goalRoot, themeDir, GOALROOTLEN - strlen(goalRoot) - 1);
+
+    readTheme(goalRoot);
+
     strncpy(goalPath, goalRoot, GOALPATHLEN-1);
     strncat(goalPath, GOALDIR, GOALPATHLEN - strlen(goalPath) - 1);
-    Serial.print(F("the goal dir is: "));
-    Serial.println(goalPath);
-    goalSounds = listFiles(goalPath, &goalSoundsNum);
+
+    randomSeed(analogRead(UNUSED_ANALOG_PIN));
+    getSoundFilePath(goalFilePath, goalPath, goalSoundIndices[random(0, goalSoundCount-1)]);
+
+    Serial.print(F("goal sound path is: "));
+
+    Serial.println(goalFilePath);
+    musicPlayer.startPlayingFile(goalFilePath);
+/*
     Serial.print(F("goal sound is: "));
     Serial.println(goalSounds[0]);
     for (int j=0; j < 20; j++) {
@@ -223,6 +235,53 @@ void initSoundFiles() {
     Serial.print(F("playing: "));
     Serial.println(goalFilePath);
     musicPlayer.startPlayingFile(goalFilePath);
+*/
+}
+
+void getSoundFilePath(char *soundFilePath, char *dirName, uint16_t themeFilePosition) {
+    File themeFile = SD.open(themeFilePath, FILE_READ);
+    themeFile.seek(themeFilePosition);
+    char fileName[NAMELEN];
+    readword(fileName, themeFile, NAMELEN - 1);
+    strncpy(soundFilePath, dirName, GOALFILEPATHLEN - 1);
+    strncat(soundFilePath, fileName, GOALFILEPATHLEN - strlen(soundFilePath) - 1);
+}
+
+void readTheme(char *dirName) {
+    strncpy(themeFilePath, dirName, THEMEFILEPATHLEN - 1);
+    strncat(themeFilePath, THEMEFILENAME, THEMEFILEPATHLEN - 1);
+    File themeFile = SD.open(themeFilePath, FILE_READ);
+    if (!themeFile.available()) {
+        Serial.println(F("theme.txt not found"));
+        return;
+    }
+    goalSoundCount = fastGoalSoundCount = slowGoalSoundCount = 0;
+    const char FILELINELEN = 30;
+    char fileLine[FILELINELEN];
+    uint16_t *currentlyReading;
+    uint8_t *currentlyCounting;
+    uint16_t lineStartPosition;
+    uint8_t i;
+    while (themeFile.available()) {
+        lineStartPosition = themeFile.position();
+        readline(fileLine, themeFile, FILELINELEN - 1);
+        if (strncmp(fileLine, "#", 1) == 0) {
+            continue;
+        } else if (strncmp(fileLine, GOALDIR, 5) == 0 && strlen(fileLine) == 5) {
+            currentlyReading = goalSoundIndices;
+            currentlyCounting = &goalSoundCount;
+        } else if (strncmp(fileLine, FASTGOALDIR, 9) == 0) {
+            currentlyReading = fastGoalSoundIndices;
+            currentlyCounting = &fastGoalSoundCount;
+        } else if (strncmp(fileLine, SLOWGOALDIR, 9) == 0) {
+            currentlyReading = slowGoalSoundIndices;
+            currentlyCounting = &slowGoalSoundCount;
+        } else {
+            currentlyReading[*currentlyCounting] = lineStartPosition;
+            (*currentlyCounting)++;
+        }
+    }
+    themeFile.close();
 }
 
 void readline(char *line, File file, uint8_t maxlen) {
@@ -237,6 +296,20 @@ void readline(char *line, File file, uint8_t maxlen) {
         i++;
     }
     line[i] = '\0';
+}
+
+void readword(char *word, File file, uint8_t maxlen) {
+    uint8_t i = 0;
+    char c;
+    while (i < maxlen && file.available()) {
+        c = (char)file.read();
+        if (c == ' ' || c == ',' || c == '\t' || c == '\r' || c == '\n') {
+            break;
+        }
+        word[i] = c;
+        i++;
+    }
+    word[i] = '\0';
 }
 
 char ** listFiles(char *dirName, uint8_t *numFiles) {
